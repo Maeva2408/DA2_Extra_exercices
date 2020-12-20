67bd12ef40bd2a3a85ec63189874c201e7ecdb11
Maeva Braeckevelt <Braeckevelt_maeva@student.ceu.edu>
2020-12-20 19:25:29 +0100

    finishing exercices 3

--- Code/Analysis_hotels_brussels.R
+++ Code/Analysis_hotels_brussels.R
@@ -1,40 +1,18 @@
 #######################
-## Assignement for   ##
-## DA2 and COD1      ##
-## Analysis of       ##
-##  number of        ##
-## registered death  ##
-## and number of     ##
-## registered case $ ##
-## due to covid on   ##
-##  the 25/10/2020   ##
-##                   ##
-## Analysis of       ##
-##   the data        ##
+## Extra exercices   ##
+## DA2               ##
+## exercices 3 chap7 ##
+##  Brussels hotels  ##
 ##                   ##
 #######################
 
-### Task : Analyse the pattern of association between registered covid-19 
-### cases and registered number of death du to covid-19 on the 26 october 2020.
+### Task : Analyse the pattern of association between the price of hotels in 
+## Brussels with the distance from the city center
 
 # Clear memory
 rm(list=ls())
 
 # Packages to use
-library(tidyverse)
-# For scaling ggplots
-require(scales)
-# Estimate piecewise linear splines
-#install.packages("lspline")
-library(lspline)
-# Estimate robust SE
-#install.packages("estimatr")
-library(estimatr)
-# Compare models with robust SE
-#install.packages("texreg")
-library(texreg)
-# For different themes
-#install.packages(ggthemes)
 library(ggthemes)
 library(tidyverse)
 library(geosphere)
@@ -42,15 +20,15 @@ library(moments)
 library(dplyr)
 library(knitr)
 library(pander)
-install.packages("jtools")
-install.packages("huxtable")
 library(huxtable)
 library(jtools)
+library(ggplot2)
+library(texreg)
+library(estimatr)
+
 # First, I will import ly clean data from github
 my_url <- "https://raw.githubusercontent.com/Maeva2408/DA2_Extra_exercices/main/data/clean/hotel_Brussels.csv"
 brussels <- read_csv( my_url )
-
-
 
 ######
 
@@ -70,6 +48,15 @@ hotels_brussels <- filter( brussels ,
                           stars >= 3 & stars <= 4, distance <= 5,
                           price <= 400)
 
+hotels_brussels %>% 
+  keep(is.numeric) %>%
+  ggplot(aes(x= distance))+
+  geom_histogram(fill= "orangered4", col= "salmon")+
+  theme_bw()+
+  scale_fill_wsj()+
+  labs(x = "Distance from the city center (miles))",y = "count of hotels")
+
+# Summary statistics of the price
 df_hotels_brussels <- summarise(hotels_brussels,
                              variable = "price",
                              mean = mean(x = price),
@@ -83,33 +70,40 @@ df_hotels_brussels <- summarise(hotels_brussels,
 kable(df_hotels_brussels)
 
 
-
-
-hotels_brussels %>% 
-  keep(is.numeric) %>%
-  ggplot(aes(x= distance))+
-  geom_histogram(fill= "orangered4", col= "salmon")+
-  theme_bw()+
-  scale_fill_wsj()+
-  labs(x = "Distance from the city center (miles))",y = "count of hotels")
-
 ## Non parametric regression
 # Bin scatter with four bins
-hotels_brussels %>% ggplot(aes(x = distance, y = price)) +
-  geom_point( size = 3)+
-  stat_summary_bin(fun ='mean', bins = 4, color='salmon', size= 7, geom='point')
-  labs(x='Distance (miles)',y='price ($)')
-
-## Lowess regression
 # Scatter plot
+
+scatter_brussels <- ggplot(data = hotels_brussels) +
+  geom_point(aes(x = distance, y = price), color = 'salmon') + 
+  expand_limits(x = 0.01, y = 0.01) +
+  scale_x_continuous(expand = c(0.01,0.01), limits=c(0, 7), breaks=seq(0, 7, by=1)) + 
+  scale_y_continuous(expand = c(0.01,0.01),limits = c(0, 400), breaks = seq(0, 400, by = 50)) +
+  labs(x = "Distance to city center (miles)",y = "Price (dollars)")+
+  theme_bw() 
+scatter_brussels
+
+# Four bins : between 0,5 and 1,5 miles, between 1,5 and 2,5, between 2,5 and 3,5, between 3,5 and 4,5;
+hotels_brussels <-hotels_brussels %>% mutate(dist4=0.5+ 1*as.numeric(hotels_brussels$distance>=1) 
+                + 1*as.numeric(hotels_brussels$distance>=2) + 1*as.numeric(hotels_brussels$distance>=3))
+dist4 <- hotels_brussels %>% group_by(dist4) %>% dplyr::summarize(Eprice_cat4=mean(price))
+hotels_brussels<-left_join(hotels_brussels,dist4)
+
+hotels_brussels$xend <- c(hotels_brussels$dist4+1)
+hotels_brussels$yend <- c(hotels_brussels$Eprice_cat4)
+
+bins_hotels <-  scatter_brussels+
+  geom_segment(data=hotels_brussels, aes(x = dist4, y=yend, xend=xend, yend=yend)) 
+bins_hotels
+  
+## Lowess regression
+# Approximate with a lo(w)ess non-parametric line
 scatter <- ggplot( hotels_brussels , aes(x = distance, y = price )) +
   geom_point(fill= "orangered4", col= "salmon"  )+
   theme_bw()+
   scale_fill_wsj()+
   labs(x='Distance (miles)',y='Price ($)')
 
-
-# Approximate with a lo(w)ess non-parametric line
 scatter + geom_smooth(method=loess,se=F,formula='y~x',color='black')
 
 
